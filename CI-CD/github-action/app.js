cfb3425f5cc851f861a7f3611158590b0adfe79f
Eliyahu Hildesheim <4167101@gmail.com>
2026-06-22 10:21:01 +0300

    Update app.js

--- CI-CD/github-action/app.js
+++ CI-CD/github-action/app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Lets build the infrastructure of githab actions and GitOps 0806 \n');
+  res.end('Lets build the infrastructure of githab actions aaa and GitOps 0806 \n');
 });
 
 server.listen(port, hostname, () => {
